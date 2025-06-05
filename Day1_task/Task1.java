479a92a3e7649f8bb3b3422fdc77bb73947100b2
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-05 12:53:26 +0530

    Day1 completed

---
+++ Day1_task/Task1.java
@@ -0,0 +1,18 @@
+package add_DSA;
+
+import java.util.Arrays;
+
+public class Task1 {
+	public static void main(String[] args) {
+		int[] arr= {12,13,14,15,16};
+		System.out.println(Arrays.toString(arr));
+		for(int i=0;i<5;i++) {
+			System.out.println(arr[i]);
+		}
+		
+	
+			
+		
+	}
+
+}
